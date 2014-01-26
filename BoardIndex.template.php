941476297bd238c779c49e2ffdf62e9b0cafba6f
CeeMoo <qdale.123@gmail.com>
2014-01-26 19:02:43 +0200

    p atama

--- BoardIndex.template.php
+++ BoardIndex.template.php
@@ -71,12 +71,10 @@ function bilgi(){
 	if ($settings['show_stats_index'])
 	{
 		echo ' 
-			<p>
 				', $context['common_stats']['total_posts'], ' ', $txt['posts_made'], ' ', $txt['in'], ' ', $context['common_stats']['total_topics'], ' ', $txt['topics'], ' ', $txt['by'], ' ', $context['common_stats']['total_members'], ' ', $txt['members'], '. ', !empty($settings['show_latest_member']) ? $txt['latest_member'] . ': <strong> ' . $context['common_stats']['latest_member']['link'] . '</strong>' : '', '<br />
 				', (!empty($context['latest_post']) ? $txt['latest_post'] . ': <strong>&quot;' . $context['latest_post']['link'] . '&quot;</strong>  ( ' . $context['latest_post']['time'] . ' )<br />' : ''), '
 				<a href="', $scripturl, '?action=recent">', $txt['recent_view'], '</a>', $context['show_stats'] ? '<br />
-				<a href="' . $scripturl . '?action=stats">' . $txt['more_stats'] . '</a>' : '', '
-			</p>';
+				<a href="' . $scripturl . '?action=stats">' . $txt['more_stats'] . '</a>' : '', '';
 	}
 
 
